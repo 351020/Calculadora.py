6267fe9b5d1ce4cb4310a493a45efc14d5866962
Marco Luis <47786617+351020@users.noreply.github.com>
2022-06-23 09:48:22 -0400

    Update Caculadora.py

--- Caculadora.py
+++ Caculadora.py
@@ -3,7 +3,7 @@ while True:
     num_2 = input("Digite outro número: ")
     operador = input('Digite o Operador: ')
 
-    sair = input('Deseja sair? Digite [s]im ou [n]ão:  ')
+    sair = input('Deseja sair? Digite [s]im ou [n]ão:  ').lower()
 
     if not num_1.isnumeric() or not num_2.isnumeric():
         print('Número Invalido Digite o Número corretamente. ')
